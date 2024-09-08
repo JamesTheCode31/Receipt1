f74ef23a9442b4f470a453b3935a452948339064
JamesTheCode31 <jsn8340@students.uc-bcf.edu.ph>
2024-09-08 23:36:07 +0800

    Add files via upload

---
+++ Invoice.java
@@ -0,0 +1,22 @@
+public class Invoice {
+	public static void main (String []args){
+			//Store Information
+			System.out.println("Joystick Arena\t\t\t\t\t08/09/24");
+			System.out.print("SM City Baguio, Luneta Hill Dr, 231, Baguio, 2600 Benguet");
+			
+			//Items List:
+			System.out.println("\n________________________________________________________________________");
+			System.out.println("\n<Item>\t\t\t\t\t\t<Quality>\t<Prices>");
+			System.out.println("\n________________________________________________________________________");
+			System.out.println("FIFA 23-PlayStation 5\t\t\t\t1\t\t$34.00 ");
+			System.out.println("Minecraft Legends-Deluxe Edition\t\t1\t\t$50.00 ");
+			System.out.println("PlayStation 5\t\t\t\t\t1\t\t$499");
+			System.out.println("Ps5 Wireless Controllers\t\t\t1\t\t$69.99");
+			System.out.println("Ps5 Wireless Earbuds\t\t\t\t1\t\t$199.99");
+			System.out.println("_________________________________________________________");
+			System.out.println("\n\t\t\t\t\t\t<Total>\t\t$852.98");
+			//Thank You Message
+			System.out.print("\nThank you for shopping at Joystick Arena, where you are the gamer in your own world. Visit us again!");
+			
+	}
+}
